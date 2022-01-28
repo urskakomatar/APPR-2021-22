da19fd0dbb0a66e0da647061566a6fd8747eaa9e
Urska Komatar <urskak2000@gmail.com>
2022-01-28 01:29:45 +0100

    popravljanje vizualizacije in analize. začetek rmd

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,1 +1,31 @@
 # 4. faza: Napredna analiza podatkov
+
+#za leto 2019
+
+library(ggalt)
+#skupine <- data.frame(Gospodarske_značilnosti$bdppc,Gospodarske_značilnosti$indeks);
+#diagram.skupine = function(podatki, oznake, skupine, k) {
+#  podatki = podatki %>%
+#    bind_cols(skupine)%>% rename(Država = ...4)
+  
+#  d = podatki %>%
+ #   ggplot(
+  #    mapping = aes(
+   #     x = bdppc, y = indeks, color = Država
+#      )
+ #   ) +
+  #  geom_point() +
+   # geom_label(label = oznake, size = 2) +
+    #scale_color_hue() +
+#    theme_classic()
+  
+#  for (i in 1:k) {
+ #   d = d + geom_encircle(
+  #    data = podatki %>%
+   #     filter(Država == i)
+#    )
+#  }
+#  d
+#}
+
+
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -7,16 +7,18 @@ library(plotly)
 library(sf)
 ######Število potovanj glede na spol EU#####
 
-EU_Spol%>% ggplot(mapping = aes(x = leto, y = stevilo ,color = spol, group=1))+ geom_line()  + geom_point()
 
-ggplot(EU_Spol) + geom_line(aes(x = leto, y = stevilo, color = spol, group = spol))
+g1 <- ggplot(EU_Spol) + geom_line(aes(x = leto, y = stevilo/10^6, color = spol, group = spol))+ labs(
+  x = "Leto",
+  y = "Število ljudi v mio",
+  title = sprintf("Število potovanj glede na spol"))
  
  
 #####Število potovanj glede na starost EU###### ok 
 
-sumStarost %>% ggplot() + geom_col(data = sumStarost, aes(x = leto, y = x, fill = starost)) + labs(
+g2 <- sumStarost %>% ggplot() + geom_col(data = sumStarost, aes(x = leto, y = x/10^6, fill = starost)) + labs(
   x = "Leto",
-  y = "Število potovanj",
+  y = "Število potovanj v mio",
   title = sprintf("Število potovanj glede na starost"))
 
 
@@ -32,12 +34,16 @@ g3 <- average_indeks %>%
     title = sprintf("Število potovanj na osebo v EU")
   ) + theme(axis.text.x = element_text(angle = 60, hjust = 1))
 
-g3 + average_indeks%>%filter(Država == "EU") %>% geom_bar(
+g3 <- g3 + average_indeks%>%filter(Država == "EU") %>% geom_bar(
   mapping = aes(x = Država, y = x) ,
   stat = "identity",
   fill = "red"
 )
-
+g3 <- g3 + average_indeks%>%filter(Država == "Slovenija") %>% geom_bar(
+  mapping = aes(x = Država, y = x) ,
+  stat = "identity",
+  fill = "green"
+)
 
 #########min.plača, bdppc, št.preb. in velikost indeksa
 g4 <- Gospodarske_značilnosti[1:27,] %>%
@@ -64,27 +70,22 @@ g5 <- Gospodarske_značilnosti[1:27,] %>%
 g5 <- ggplotly(g5,tooltip = "Država")
 g5
 
-g6 <- Gospodarske_značilnosti[190:216,] %>%
-  ggplot(
-    mapping = aes(x =bdppc ,y =indeks.bp,  size = indeks , color = Država)
-  ) +
-  geom_point()
-
 ######zemljevid#########
 ###tukaj bo še tabelica indeksa, kjer bodo popravljena imena nazaj v angl. da lahko mergamo##
+data("World")
 ang_indeks <-  average_indeks
 match(ang_indeks$Država, poimenovanje_drzav$drzave)
 ang_indeks$Država = unlist(poimenovanje_drzav[match(ang_indeks$Država, poimenovanje_drzav$drzave), "drzava_anglesko"])
 ang_indeks$Država[ang_indeks$Država == "Czech Republic" ] <- "Czech Rep."
 colnames(ang_indeks)[1] <- "name"
-podatki <- ang_indeks %>% left_join(evropa, by = "name")
+podatki <- ang_indeks %>% left_join(World, by = "name")
 podatki <- podatki[!(podatki$name == "Cyprus"| podatki$name == "Malta"| podatki$name == "Slovak Republic"|
   podatki$name == "European Union - 27 countries (from 2020)"),]
-data("World")
+
 
 
 st_potovanj_graf <- function() {
-  podatki <- st_sf(ang_indeks %>% left_join(evropa, by = "name"))
+  podatki <- st_sf(ang_indeks %>% left_join(World, by = "name"))
   podatki <- podatki[!(podatki$name == "Cyprus"| podatki$name == "Malta"| podatki$name == "Slovak Republic"|
                          podatki$name == "European Union - 27 countries (from 2020)"),]
   evropa <- tm_shape(podatki) + tm_polygons('x')
@@ -92,7 +93,7 @@ st_potovanj_graf <- function() {
   return(evropa)
 } 
 
-st_potovanj_graf()
+mapa1 <- st_potovanj_graf()
 
 
 
